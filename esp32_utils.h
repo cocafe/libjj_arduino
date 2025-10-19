8496181d0efee6e7b1f4f47e192f0be056ac22e0
cocafe <cocafehj@gmail.com>
2025-10-19 13:30:45 +0800

    show internal and psram heap size
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- esp32_utils.h
+++ esp32_utils.h
@@ -169,7 +169,10 @@ static int esp32_top_stats_print(unsigned sampling_ms, int (*__snprintf)(char *b
         uint64_t total_cpu_percent = 100ULL * (total_ticks_delta - idle_ticks_delta) / total_ticks_delta;
         c += __snprintf(&buf[c], bufsz - c, "ulTotalRunTimeDelta: %lu TicksDelta: %ju IdleDelta: %ju TotalCPU: %ju%%\n",
                         rt2 - rt1, total_ticks_delta, idle_ticks_delta, total_cpu_percent);
-        c += __snprintf(&buf[c], bufsz - c, "FreeHeapBytes: %lu MinimalFreeHeap: %lu\n", esp_get_free_heap_size(), esp_get_minimum_free_heap_size());
+        c += __snprintf(&buf[c], bufsz - c, "FreeHeapBytes: %lu LowWatermark: %lu\n", heap_caps_get_free_size(MALLOC_CAP_INTERNAL), heap_caps_get_minimum_free_size(MALLOC_CAP_INTERNAL));
+
+        if (psramFound())
+                c += __snprintf(&buf[c], bufsz - c, "FreePSRAMBytes: %lu LowWatermark: %lu\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM), heap_caps_get_minimum_free_size(MALLOC_CAP_SPIRAM));
 
         free(tasks1);
         free(tasks2);
@@ -214,7 +217,10 @@ static int esp32_top_snapshot_print(int (*__snprintf)(char *buffer, size_t bufsz
         }
 
         c += __snprintf(&buf[c], bufsz - c, "ulTotalRunTime: %lu TotalTicks: %ju IdleTicks: %ju\n", rt, total_ticks, idle_ticks);
-        c += __snprintf(&buf[c], bufsz - c, "FreeHeapBytes: %lu MinimalFreeHeap: %lu\n", esp_get_free_heap_size(), esp_get_minimum_free_heap_size());
+        c += __snprintf(&buf[c], bufsz - c, "FreeHeapBytes: %lu LowWatermark: %lu\n", heap_caps_get_free_size(MALLOC_CAP_INTERNAL), heap_caps_get_minimum_free_size(MALLOC_CAP_INTERNAL));
+
+        if (psramFound())
+                c += __snprintf(&buf[c], bufsz - c, "FreePSRAMBytes: %lu LowWatermark: %lu\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM), heap_caps_get_minimum_free_size(MALLOC_CAP_SPIRAM));
 
         free(tasks);
 
