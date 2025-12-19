b9140dd3f072bcaffe525e7beaa8daad6602af7b
cocafe <cocafehj@gmail.com>
2025-12-19 20:21:00 +0800

    add arudino esp32 pm api
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- rpc_pm.h
+++ rpc_pm.h
@@ -5,9 +5,87 @@
 
 void rpc_pm_add(void)
 {
-        http_rpc.on("/pm", HTTP_GET, [](){
+#ifdef CONFIG_PM_ENABLE
+        http_rpc.on("/esp32_pm", HTTP_GET, [](){
                 if (http_rpc.hasArg("set")) {
-                        
+                        int max_freq = -1, min_freq = -1, light_slp = -1, err = 0;
+
+                        if (http_rpc.hasArg("max_freq")) {
+                                String _max_freq = http_rpc.arg("max_freq");
+
+                                if (is_str_equal((char *)_max_freq.c_str(), "max", CASELESS)) {
+                                        max_freq = ESP_PM_CPU_FREQ_MAX;
+                                } else if (is_str_equal((char *)_max_freq.c_str(), "min", CASELESS)) {
+                                        max_freq = 80;
+                                } else {
+                                        int i = strtoll_wrap(_max_freq.c_str(), 10, &err);
+
+                                        if (err || i < 0) {
+                                                http_rpc.send(500, "text/plain", "invalid value\n");
+                                                return;
+                                        }
+
+                                        max_freq = i;
+                                }
+                        }
+
+                        if (http_rpc.hasArg("min_freq")) {
+                                String _min_freq = http_rpc.arg("min_freq");
+
+                                if (is_str_equal((char *)_min_freq.c_str(), "max", CASELESS)) {
+                                        min_freq = ESP_PM_CPU_FREQ_MAX;
+                                } else if (is_str_equal((char *)_min_freq.c_str(), "min", CASELESS)) {
+                                        min_freq = 80;
+                                } else {
+                                        int i = strtoll_wrap(_min_freq.c_str(), 10, &err);
+
+                                        if (err || i < 0) {
+                                                http_rpc.send(500, "text/plain", "invalid value\n");
+                                                return;
+                                        }
+
+                                        min_freq = i;
+                                }
+                        }
+
+                        if (http_rpc.hasArg("light_sleep")) {
+                                String _light_slp = http_rpc.arg("light_sleep");
+                                int i = strtoll_wrap(_light_slp.c_str(), 10, &err);
+
+                                if (err || i < 0) {
+                                        http_rpc.send(500, "text/plain", "invalid value\n");
+                                        return;
+                                }
+
+                                light_slp = !!i;
+                        }
+
+                        if (max_freq < 0 || min_freq < 0 || light_slp < 0) {
+                                http_rpc.send(500, "text/plain", "<max_freq:max/min/num> <min_freq:max/min:num> <light_sleep:0/1>\n");
+                                return;
+                        }
+
+                        pr_info("freq max/min: %d/%d light_sleep: %d\n", max_freq, min_freq, light_slp);
+
+                        esp_pm_config_t cfg = {
+                                .max_freq_mhz = max_freq,
+                                .min_freq_mhz = min_freq,
+                                .light_sleep_enable = (bool)light_slp,
+                        };
+
+                        err = esp_pm_configure(&cfg);
+                        if (err != ESP_OK) {
+                                if (err == ESP_ERR_INVALID_ARG)
+                                        http_rpc.send(500, "text/plain", "api: invalid value\n");
+                                else if (err == ESP_ERR_NOT_SUPPORTED)
+                                        http_rpc.send(500, "text/plain", "api: not supported\n");
+                                else
+                                        http_rpc.send(500, "text/plain", "api failure\n");
+
+                                return;
+                        }
+
+                        http_rpc.send(200, "text/plain", "OK\n");
                 } else {
                         int bufsz = 512;
                         char *buf = NULL;
@@ -59,6 +137,79 @@ void rpc_pm_add(void)
                         free(buf);
                 }
         });
+#endif
+
+#ifdef Arduino_h
+        //function takes the following frequencies as valid values:
+        //  240, 160, 80    <<< For all XTAL types
+        //  40, 20, 10      <<< For 40MHz XTAL
+        //  26, 13          <<< For 26MHz XTAL
+        //  24, 12          <<< For 24MHz XTAL
+        http_rpc.on("/esp32_cpufreq", HTTP_GET, [](){
+                if (http_rpc.hasArg("set")) {
+                        static const unsigned valid_freq[] = { 240, 160, 80, 40, 20, 10, 26, 13, 24, 12 };
+                        String _freq = http_rpc.arg("set");
+                        unsigned long freq;
+                        int err, valid = 0;
+
+                        freq = strtoull_wrap(_freq.c_str(), 10, &err);
+                        if (err) {
+                                http_rpc.send(500, "text/plain", "invalid number\n");
+                                return;
+                        }
+
+                        for (unsigned i = 0; i < ARRAY_SIZE(valid_freq); i++) {
+                                if (freq == valid_freq[i]) {
+                                        valid = 1;
+                                        break;
+                                }
+                        }
+
+                        if (!valid) {
+                                char buf[128];
+                                int c = 0;
+                                c += snprintf(&buf[c], sizeof(buf) - c, "valid freq MHz: ");
+                                for (unsigned i = 0; i < ARRAY_SIZE(valid_freq); i++) {
+                                        c += snprintf(&buf[c], sizeof(buf) - c, "%u ", valid_freq[i]);
+                                }
+                                c += snprintf(&buf[c], sizeof(buf) - c, "\n");
+                                http_rpc.send(500, "text/plain", buf);
+                                return;
+                        }
+
+                        setCpuFrequencyMhz(freq);
+                        http_rpc.send(500, "text/plain", "OK\n");
+                } else {
+                        char buf[32] = { };
+
+                        unsigned freq = getCpuFrequencyMhz();
+
+                        snprintf(buf, sizeof(buf), "%u MHz\n", freq);
+
+                        http_rpc.send(200, "text/plain", buf);
+                }
+        });
+
+        http_rpc.on("/esp32_xtalfreq", HTTP_GET, [](){
+                char buf[32] = { };
+
+                unsigned freq = getXtalFrequencyMhz();
+
+                snprintf(buf, sizeof(buf), "%u MHz\n", freq);
+
+                http_rpc.send(200, "text/plain", buf);
+        });
+
+        http_rpc.on("/esp32_abpfreq", HTTP_GET, [](){
+                char buf[32] = { };
+
+                unsigned long freq = getApbFrequency();
+
+                snprintf(buf, sizeof(buf), "%lu Hz\n", freq);
+
+                http_rpc.send(200, "text/plain", buf);
+        });
+#endif
 }
 
 #endif // __LIBJJ_RPC_PM_H__
